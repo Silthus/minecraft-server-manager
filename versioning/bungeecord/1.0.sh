9338b6e8fdeafe38993ae44b400b577c87839609
Dave Fallon <davef@tetsubo.com>
2013-06-23 10:42:26 -0700

    Added "console_property" that sets values in server.properties when set_jar is called. Goal is to be able to properly setup the server.properties for a given jar/server.

--- versioning/bungeecord/1.0.sh
+++ versioning/bungeecord/1.0.sh
@@ -1,5 +1,10 @@
 # MSM version file for BungeeCord 1.0 and above
 
+console_event START:30 "Listening on"
+
+console_property msm-log-path "proxy.log.0"
+console_property msm-log-regex "^[0-9]{2}:[0-9]{2}:[0-9]{2} \[.*\]"
+
 console_command STOP "end" \
   "Proxy Stopped."
 
